64a1307eee523b993ba1a29c70f93a59598972d6
Figby222 <figby222@outlook.com>
2024-09-21 17:32:30 -0700

    Add populatedb.mjs

---
+++ db/populatedb.mjs
@@ -0,0 +1,42 @@
+import pg from "pg";
+import "dotenv/config";
+const { Client } = pg;
+
+const SQL = `CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR ( 255 ) NOT NULL,
+    last_name VARCHAR ( 255 ) NOT NULL,
+    passwordHash TEXT,
+    salt TEXT,
+    membership_status BOOLEAN DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR ( 255 ) NOT NULL,
+    text TEXT,
+    timestamp DATE,
+    creator_id INTEGER CONSTRAINT 
+        creator_id_foreign_key 
+        REFERENCES users (id)
+        ON DELETE CASCADE
+        NOT NULL
+);
+`
+
+async function main() {
+    console.log("seeding....");
+    
+    const client = new Client({
+        connectionString: process.argv[2],
+    })
+
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    
+    console.log("done");
+}
+
+
+main();
