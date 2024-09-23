e9b27dec31b635c090fe0225d151faabb5b07238
Figby222 <figby222@outlook.com>
2024-09-22 18:20:00 -0700

    Add /login GET route

--- routers/indexRouter.mjs
+++ routers/indexRouter.mjs
@@ -3,6 +3,8 @@ import * as indexController from "../controllers/indexController.mjs";
 
 const indexRouter = Router();
 
+indexRouter.get("/login", indexController.loginPageGet);
+
 indexRouter.get("/join-club", indexController.joinClubPageGet);
 
 indexRouter.get("/sign-up", indexController.signUpFormGet);
